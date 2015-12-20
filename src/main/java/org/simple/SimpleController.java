f0db46fc4d8368b94c09a14ac8b304bd3eb9f2c1
Berthold <berthold.schulte@netcologne.de>
2015-12-20 18:50:41 +0100

    init logback

--- src/main/java/org/simple/SimpleController.java
+++ src/main/java/org/simple/SimpleController.java
@@ -3,13 +3,12 @@ package org.simple;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController(value="/")
+@RestController(value = "/")
 public class SimpleController {
 
-	@RequestMapping(value="check")
+	@RequestMapping(value = "check")
 	public String healthy() {
-		
-		return "IMOK";
+		return "IMOKs";
 
 	}
 }
